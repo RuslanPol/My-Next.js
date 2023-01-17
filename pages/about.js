4e651a45e9702d504b37feb3836b7840c5a89bec
RuslanPol <103576799+RuslanPol@users.noreply.github.com>
2023-01-17 23:55:26 +0700

    new ver

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+export default function AboutPage(){
+    return<> <h1>Welcome, to AboutPage</h1>
+    <a href="/">Home</a></>
+    
+}
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+export default function HomePage(){
+    return <><h1>Welcome, to IndexPage</h1>
+    <a href="about">About</a></>
+
+}
